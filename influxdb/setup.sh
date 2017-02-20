f1fd525b602bdb4b4b2676b01ff70662275ec10c
szukalski <szukalski@gmail.com>
2017-02-20 15:05:09 +0100

    zoo

--- influxdb/setup.sh
+++ influxdb/setup.sh
@@ -8,6 +8,8 @@ PERSISTENT_VOLUMES=(var/lib/influxdb)
 PERSISTENT_FILES=(etc/influxdb/influxdb.conf)
 VOLUMES=""
 DATABASES=("servers" "virtualhosts" "network")
+ADMIN_USER=dba
+ADMIN_PASSWORD=dbapassword
 
 # Create persistent volume locations if they don't exist
 for VOL in ${PERSISTENT_VOLUMES[@]}
@@ -23,8 +25,11 @@ for FILE in ${PERSISTENT_FILES[@]}
 do
 	VOLUMES=$VOLUMES" -v $PERSISTENT_LOCATION/$FILE:/$FILE"
 	if [ ! -f $PERSISTENT_LOCATION/$FILE ]; then
-	mkdir -p $PERSISTENT_LOCATION/$FILE
+		mkdir -p $PERSISTENT_LOCATION/$(dirname $FILE)
+	else
+		rm $PERSISTENT_LOCATION/$FILE
 	fi
+	cp $PWD/$FILE $PERSISTENT_LOCATION/$FILE
 done
 
 # Create persistent storage container
@@ -40,8 +45,11 @@ docker run -d \
 
 sleep 10
 
+# Create admin
+curl --noproxy localhost -G http://localhost:8086/query --data-urlencode "q=CREATE USER $ADMIN_USER WITH PASSWORD 'dbapassword' WITH ALL PRIVILEGES"
+
 # Create databases
 for DB in "${DATABASES[@]}"
 do
-  curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE $DB"
+  curl --noproxy localhost -G http://localhost:8086/query -u $ADMIN_USER:$ADMIN_PASSWORD --data-urlencode "q=CREATE DATABASE $DB"
 done
