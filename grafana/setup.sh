12b4b9bd55b2fb58d35cee66b6071522e9f5d386
szukalski <szukalski@gmail.com>
2017-02-20 13:26:02 +0100

    zoo

--- grafana/setup.sh
+++ grafana/setup.sh
@@ -4,14 +4,15 @@ IMAGE=grafana/grafana
 CONTAINER=grafana
 PERSISTENT_LOCATION=/srv/docker/$CONTAINER
 PERSISTENT_VOLUMES=(var/lib/grafana etc/grafana)
+VOLUMES=""
 
 # Pull the image
-docker pull $IMAGE
+#docker pull $IMAGE
 
 # Create persistent volume locations if they don't exist
 for VOL in ${PERSISTENT_VOLUMES[@]}
 do
-	echo $VOL
+	VOLUMES=$VOLUMES" -v $PERSISTENT_LOCATION/$VOL:/$VOL"
 	if [ ! -d $PERSISTENT_LOCATION/$VOL ]; then
 		mkdir -p $PERSISTENT_LOCATION/$VOL
 	fi
@@ -20,7 +21,7 @@ done
 # Run the image
 docker run -d \
 	-p 3000:3000 \
-	-v $PWD/var/lib/grafana:/var/lib/grafana \
+	$VOLUMES \
 	-e "GF_USERS_ALLOW_SIGN_UP=false" \
 	-e "GF_USERS_ALLOW_ORG_CREATE=false" \
 	-e "GF_SECURITY_ADMIN_PASSWORD=secret" \
