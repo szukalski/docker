089443b4e9fd0ceefe0c66cdcec650039139e106
szukalski <szukalski@gmail.com>
2017-02-27 16:52:06 +0100

    zoo

--- graf/run.sh
+++ graf/run.sh
@@ -25,3 +25,9 @@ do
 	curl --noproxy localhost -G http://localhost:8086/query -u $INFLUX_ADMIN:$INFLUX_ADMIN_PASSWORD --data-urlencode "q=GRANT READ ON $DB to $INFLUX_USER"
 done
 
+# Add datasource
+curl --noproxy localhost "http://$GRAFANA_ADMIN:$GRAFANA_ADMIN_PASSWORD@localhost:3000/api/datasources" \
+	-X POST \
+	-H 'Content-Type: application/json;charset=UTF-8' \
+	--data-binary \
+	'{"name":"influx","type":"influxdb","url":"http://influxdb:8086","access":"proxy","isDefault":true,"database":"influx","user":"$INFLUX_USER","password":"$INFLUX_USER_PASSWORD"}'
