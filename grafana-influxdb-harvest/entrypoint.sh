dd28c951703ca9932964fad749a25868e9a8f1e1
szukalski <szukalski@gmail.com>
2017-03-02 09:22:07 +0100

    zoo

--- grafana-influxdb-harvest/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/etc/init.d/netapp-harvest start
-
-while :; do
-	sleep 3000
-done
-
--- harvest/build.sh
+++ harvest/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # You can't include the closed-source netapp files in public so if you don't have them then present then exit..
-if [ -f ./netapp-harvest_*_all.deb ]; then
+if [ ! -f ./netapp-harvest_*_all.deb ]; then
 	echo "Missing netapp-harvest package, please download manually from NetApp and include in the directory"
 	exit 1
 fi
-if [ -f ./netapp-manageability-sdk-*.zip ]; then
+if [ ! -f ./netapp-manageability-sdk-*.zip ]; then
 	echo "Missing netapp-manageability-sdk zip, please download manually from NetApp and include in the directory"
 	exit 1
 fi
