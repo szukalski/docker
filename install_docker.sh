bdd138803922f5a4bfd042d3cc8cd02fbe589705
David James <szukalski@gmail.com>
2017-07-03 15:07:27 +0200

    werewolf

--- install_docker.sh
+++ install_docker.sh
@@ -3,20 +3,32 @@
 
 # Configure docker repo
 apt-get update
-apt-get install apt-transport-https ca-certificates -y
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 apt-get update
 
 # Install pre-reqs
 apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
 
 # Install docker and start service
-apt-get install docker-engine -y
+apt-get install docker-ce -y
 service docker start
+systemctl enable docker
+
+# Configure docker users
+#groupadd docker
+#usermod -aG docker $USER
 
 # Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.11.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # All done
