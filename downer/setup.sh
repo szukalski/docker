ccc158a2ec6da544b2adb203d03165bb332d5af7
szukalski <szukalski@gmail.com>
2018-04-25 20:45:31 +0200

    de

---
+++ downer/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install apache2-utils
+
+read -p "Directory to store volumes: "
+echo DIR="$REPLY" > .env
+
+echo UID=$(id -u) >> .env
+echo GID=$(id -g) >> .env
+cat /etc/timezone >> .env
+
