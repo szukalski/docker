c4757dc641c5815c19719183760bb378de07ce8e
szukalski <szukalski@gmail.com>
2018-04-25 20:56:02 +0200

    te

--- downer/setup.sh
+++ downer/setup.sh
@@ -3,6 +3,11 @@
 sudo apt install apache2-utils
 
 read -p "Directory to store volumes: "
+if [ ! -d $REPLY ]; then
+    echo "Directory must exist"
+    exit 1
+fi
+
 echo DIR="$REPLY" > .env
 
 echo UID=$(id -u) >> .env
